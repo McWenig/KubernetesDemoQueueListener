966c8c1b0f963556d9843c44900320b94480f02c
Martin Wenig <martin.wenig@accenture.com>
2018-05-16 15:35:42 +0200

    clean up

--- src/main/java/com/accenture/kubernetesdemo/queueapp/controller/ProtocolController.java
+++ src/main/java/com/accenture/kubernetesdemo/queueapp/controller/ProtocolController.java
@@ -1,5 +1,7 @@
 package com.accenture.kubernetesdemo.queueapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,62 +13,18 @@ import com.accenture.kubernetesdemo.queueapp.domain.Protocol;
 @Controller
 public class ProtocolController {
 
-  /**
-   * Create a new protocol with an auto-generated id and email and name as passed 
-   * values.
-   */
-  @RequestMapping(value="/protocol/create")
-  @ResponseBody
-  public String create(String name, Long actId) {
-    try {
-      Protocol protocol = new Protocol(name, actId);
-      protocolDao.create(protocol);
-    }
-    catch (Exception ex) {
-      return "Error creating the protocol: " + ex.toString();
-    }
-    return "Protocol succesfully created!";
-  }
-  
-  /**
-   * Delete the protocol with the passed id.
-   */
-  @RequestMapping(value="/protocol/delete")
-  @ResponseBody
-  public String delete(long id) {
-    try {
-      Protocol protocol = new Protocol(id);
-      protocolDao.delete(protocol);
-    }
-    catch (Exception ex) {
-      return "Error deleting the protocol: " + ex.toString();
-    }
-    return "Protocol succesfully deleted!";
-  }
-  
-    
-  /**
-   * Update the email and the name for the protocol indentified by the passed id.
-   */
-  @RequestMapping(value="/update")
-  @ResponseBody
-  public String updateName(long id, String name) {
-    try {
-      Protocol protocol = protocolDao.getById(id);
-      protocol.setName(name);
-      protocolDao.update(protocol);
-    }
-    catch (Exception ex) {
-      return "Error updating the protocol: " + ex.toString();
-    }
-    return "Protocol succesfully updated!";
-  } 
+	/**
+	 * Create a new protocol with an auto-generated id and email and name as
+	 * passed values.
+	 */
+	@RequestMapping(value = "/protocols")
+	@ResponseBody
+	public List<Protocol> list() {
+		List<Protocol> actList = protocolDao.getAll();
+		return actList;
+	}
 
+	@Autowired
+	private ProtocolDao protocolDao;
 
-  // Private fields
-  
-  // Wire the ProtocolDao used inside this controler.
-  @Autowired
-  private ProtocolDao protocolDao;
-  
 }
--- src/main/java/com/accenture/kubernetesdemo/queueapp/controller/TestController.java
+++ src/main/java/com/accenture/kubernetesdemo/queueapp/controller/TestController.java
@@ -1,15 +1,12 @@
 package com.accenture.kubernetesdemo.queueapp.controller;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 import com.accenture.kubernetesdemo.queueapp.dao.ProtocolDao;
 import com.accenture.kubernetesdemo.queueapp.domain.Protocol;
@@ -18,27 +15,16 @@ import com.accenture.kubernetesdemo.queueapp.domain.Protocol;
 public class TestController {
 
 	@Autowired
-	private RestTemplateBuilder restTemplateBuilder;
-	
-	@Autowired
-	private ProtocolDao protocollDao;
+	private ProtocolDao protocolDao;
 	
 	@RequestMapping(path="hello")
 	public String doSometing(){
-		return "Hello World, I'm minikubetest2";
-	}
-	
-	
-	@RequestMapping(path="helloOthers")
-	public String doSometingRemote() throws RestClientException, URISyntaxException{
-		RestTemplate template = restTemplateBuilder.build();
-		String result = template.getForObject(new URI("http://minikubetest1:8080/hello"), String.class);
-		return "Other says: "+result;
+		return "Hello World, I'm listening";
 	}
 	
 	@RequestMapping(path="helloDatabase")
 	public List<Protocol> doSometingDataish() throws RestClientException, URISyntaxException{
-		List<Protocol> actList = protocollDao.getAll();
+		List<Protocol> actList = protocolDao.getAll();
 		return actList;
 	}
 }
